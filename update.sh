2d9feaa0775ef1126b820af068abddf3d227340d
Tarulia <mihawk.90+git@googlemail.com>
2021-05-02 00:37:11 +0200

    misc: add update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd obs-studio
+git checkout master
+git pull
+git checkout 27.0.0-rc2 # $(cat $spec | grep Version\: | cut -d" " -f9)
+git submodule update
+git submodule status | tee ../submodules.txt
+cd ..
+
+# update commits in spec file
+sed --in-place "s/%global commit_vst .*/%global commit_vst $(cat submodules.txt | grep obs-vst | cut -d' ' -f2 )/" obs-studio.spec
+sed --in-place "s/%global commit_browser .*/%global commit_browser $(cat submodules.txt | grep obs-browser | cut -d' ' -f2 )/" obs-studio.spec
+
+rm submodules.txt
+
+if [ "$1" == "cef" ]; then
+	sed --in-place "s/%global version_cef .*/%global version_cef $2/" obs-studio.spec
+fi
+
+git diff obs-studio.spec
+
