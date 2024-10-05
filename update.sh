5f2935c6e1e8dd5032e2abd8d4710acc3d2f7155
Tarulia <mihawk.90+git@googlemail.com>
2024-10-05 03:01:35 +0200

    build: cleanup junk from update script
    * Qt5 hasn't been around for a while
    * use rpmdev-bumpspec instead of sed shenanigans

--- update.sh
+++ update.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
 spec=./obs-studio.spec
-mver=$(cat $spec | grep Version\: | cut -d" " -f9)
-rver=$(cat $spec | grep Release\: | cut -d" " -f9 | sed -e 's/%{?dist}//')
-
-if [ "$1" == "qt" ]; then
-	rpmdev-bumpspec -c "Rebuild for new Qt5" $spec
-#	git diff $spec
-	git add $spec
-	git commit -m "Rebuild for new Qt5"
-	git tag "v$mver-$rver"
-	git show HEAD
-	exit
-fi
 
 pushd obs-studio
 git reset --hard
@@ -24,16 +12,15 @@ git submodule update --init --recursive
 git submodule status
 popd
 
-# update commits in spec file
 obsVer=$(echo "$obsVer" | sed "s/-/~/")
-sed --in-place "s/Version:.*/Version:        $obsVer/" $spec
+# bumpspec writes both the Version and Changelog automatically
+rpmdev-bumpspec -n $obsVer -c "Update to $obsVer" $spec
+# bumpspec always resets to 1 for -n, but we still want to use 11
 sed --in-place "s/Release:.*/Release:        11%{?dist}/" $spec
 
 if [ "$1" == "cef" ]; then
 	sed --in-place "s/%global version_cef .*/%global version_cef $2/" $spec
 fi
-
-sed --in-place "s/%changelog/%changelog\n\* $(date +'%a %b %d %Y') Tarulia <mihawk.90+git@googlemail.com> - $obsVer-11\n- Update to $obsVer\n/" $spec
 
 git add $spec
 git diff --staged
